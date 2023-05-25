7d5b18ec76f1617facd287f2694b2953b8214176
Anton Fougner <fougneranton@gmail.com>
2023-05-25 13:49:11 +0200

    added communication between apis

--- src/main/java/com/example/demo/Controllers/OrdersController.java
+++ src/main/java/com/example/demo/Controllers/OrdersController.java
@@ -4,12 +4,15 @@ import com.example.demo.Models.Orders;
 import com.example.demo.Repositories.OrdersRepository;
 import com.example.demo.Service.OrdersService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/order")
 public class OrdersController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     private final OrdersService service;
 
@@ -31,5 +34,17 @@ public class OrdersController {
     public String addOrderWithCurl(@RequestBody Orders order) {
         return service.addNewOrder(order);
 
+    }
+
+    @GetMapping("/customer/{id}")
+    public @ResponseBody String getCustomerNameByOrderId(@PathVariable Long id){
+        String userServiceBaseUrl = "http://customerapiservice:8080/api/v1/customers/";
+        Orders order = service.getOrderById(id);
+        String userResourceUrlGetExists = userServiceBaseUrl + "exists/" + order.getCustomerId();
+        boolean exists = restTemplate.getForObject(userResourceUrlGetExists,boolean.class);
+        if(exists){
+            return "Customer med id: " + order.getCustomerId() + " har köpt order med id: " + order.getId();
+        }
+        return "Den kunden finns inte.";
     }
 }
